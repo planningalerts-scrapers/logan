0c66b4f56d324f8796020e253324c7b751ff8c78
Matthew Landauer <mlandauer@gmail.com>
2019-06-13 12:46:41 +1000

    No need to save comment_url

--- scraper.rb
+++ scraper.rb
@@ -24,7 +24,7 @@ case ENV['MORPH_PERIOD']
 end
 puts "Getting data in `" + ENV['MORPH_PERIOD'] + "`, changable via MORPH_PERIOD environment"
 
-def scrape_page(page, comment_url)
+def scrape_page(page)
   page.at("table.rgMasterTable").search("tr.rgRow,tr.rgAltRow").each do |tr|
     tds = tr.search('td').map{|t| t.inner_html.gsub("\r\n", "").strip}
     day, month, year = tds[2].split("/").map{|s| s.to_i}
@@ -34,8 +34,7 @@ def scrape_page(page, comment_url)
       "date_received" => Date.new(year, month, day).to_s,
       "description" => tds[3].gsub("&amp;", "&").split("<br>")[1].to_s.squeeze(" ").strip,
       "address" => tds[3].gsub("&amp;", "&").split("<br>")[0].gsub("\r", " ").gsub("<strong>","").gsub("</strong>","").squeeze(" ").strip,
-      "date_scraped" => Date.today.to_s,
-      "comment_url" => comment_url
+      "date_scraped" => Date.today.to_s
     }
     puts "Saving record " + record['council_reference'] + " - " + record['address']
 #      puts record
@@ -62,7 +61,6 @@ def click(page, doc)
 end
 
 url = "http://pdonline.logan.qld.gov.au/MasterViewUI/Modules/ApplicationMaster/default.aspx?page=found" + period + "&4a=&6=F"
-comment_url = "mailto:council@logan.qld.gov.au"
 
 agent = Mechanize.new
 
@@ -81,7 +79,7 @@ next_page_link = true
 
 while next_page_link
   puts "Scraping page #{current_page_no}..."
-  scrape_page(page, comment_url)
+  scrape_page(page)
 
   current_page_no += 1
   next_page_link = page.at(".rgPageNext")
